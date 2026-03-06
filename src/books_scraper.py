1cf2485e7301e8bfc3e3babb01453a8b7107057a
MaryiaKhomchanka <maryiakhomchanka2005@gmail.com>
2026-03-06 20:57:18 +0200

    Implement working version of book scraper

--- src/books_scraper.py
+++ src/books_scraper.py
@@ -1,5 +1,4 @@
 import requests 
-import csv 
 import pandas as pd 
 from bs4 import BeautifulSoup 
 
@@ -9,25 +8,38 @@ def scrape_books(url):
     books = soup.find_all('article', class_ = 'product_pod')
     return books  
 
-def books_to_dataset(books):
+
+def books_to_dataframe(books):
     books_dict = {"Title": [], "Price": [], "Rating": [], "Availability":[]}
     for book in books: 
         books_dict["Title"].append(book.find('h3').text)
         books_dict["Price"].append(book.find('p', class_ = 'price_color').text)
         books_dict["Rating"].append(book.find('p', class_ = 'star-rating')['class'][1])
-        books_dict["Availability"].append(book.find('p', class_ = 'availability').text)
-    return books_dict 
+        books_dict["Availability"].append(book.find('p', class_ = 'availability').text.strip())
+    
+    books_dataframe = pd.DataFrame.from_dict(books_dict)
+    return books_dataframe
+
+
+def get_all_books(first_page, last_page):
+    all_books = []
+    for i in range (first_page, last_page + 1):
+        url = "https://books.toscrape.com/catalogue/page-" + str(i) + ".html"
+        books = scrape_books(url)
+        all_books.extend(books)
+    all_books_dataframe = books_to_dataframe(all_books)
+    return all_books_dataframe
+
+
+def save_to_csv(dataframe):
+    dataframe.to_csv("data/books.csv")
 
 
 
 def main(): 
-    URL = "https://books.toscrape.com" 
-    books = scrape_books(URL) 
+    books = get_all_books(1, 50)
+    save_to_csv(books)
 
-    ##print(books)
-
-    booksdict = books_to_dataset(books)
-    print(booksdict)
 
 if __name__ == "__main__": 
     main()
